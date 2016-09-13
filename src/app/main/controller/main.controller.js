76ac3ff4ae0063317d6c6cf66962a178fa726fca
Luis Ramos <oxxfreddyxxo@gmail.com>
2016-09-13 01:03:12 -0300

    avances

--- src/app/main/controller/main.controller.js
+++ src/app/main/controller/main.controller.js
@@ -9,6 +9,22 @@
   function MainController() {
     var vm = this;
 
+    vm.tabs = [{
+            title: 'Nuevas Guías',
+            url: 'nuevas'
+        }, {
+            title: 'Guías de Mi Interes',
+            url: 'interes'
+        }];
+
+    vm.currentTab = 'nuevas';
+
+    vm.onClickTab = function (tab) {
+        vm.currentTab = tab.url;
+    }
     
+    vm.isActiveTab = function(tabUrl) {
+        return tabUrl == vm.currentTab;
+    }
   }
 })();
