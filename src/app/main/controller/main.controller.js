c72afec90edd71b905571a7da6fbf0f056b0b4a7
LuisKabaL <luiskabals@gmail.com>
2016-09-26 16:07:48 -0300

    avances guides

--- src/app/main/controller/main.controller.js
+++ src/app/main/controller/main.controller.js
@@ -4,11 +4,13 @@
   angular
     .module('eduMed')
     .controller('MainController', MainController);
-
+    MainController.$inject=['$scope','$state','guidesFactory','commonService']
   /** @ngInject */
-  function MainController() {
-    var vm = this;
+  function MainController($scope,$state,guidesFactory,commonService) {
 
+    var vm = this;
+    vm.newGuides=[];
+    vm.guidesOfInterest=[];
     vm.tabs = [{
             title: 'Nuevas Guías',
             url: 'nuevas'
@@ -19,6 +21,15 @@
 
     vm.currentTab = 'nuevas';
 
+    $scope.$watch('main.currentTab',function () {
+      // body...
+      console.log(vm.currentTab);
+      loadGuides(vm.currentTab);
+    });
+
+    loadGuides(vm.currentTab);
+
+
     vm.onClickTab = function (tab) {
         vm.currentTab = tab.url;
     }
@@ -26,5 +37,46 @@
     vm.isActiveTab = function(tabUrl) {
         return tabUrl == vm.currentTab;
     }
+
+    vm.isComplete = function(guide){
+            return guide.avance && guide.avance.completado;
+        };
+     vm.getImage = function(guide){
+            return commonService.getFileUrl(guide.pathImgPreview);
+        };
+
+    function loadGuides(current){
+      var response = current ==='nuevas' ? guidesFactory.getNewGuides(): guidesFactory.getGuidesOfInterest() ;
+      response.then(
+
+            function(data){
+              if(current=='nuevas'){
+                vm.newGuides=[];
+                vm.newGuides=data;
+                console.log("Guias Llamadas "+vm.newGuides);
+          
+              }else{
+                 vm.guidesOfInterest=[];
+                 vm.guidesOfInterest=data;
+              console.log("Guias Llamadas "+vm.guidesOfInterest);
+              }
+              console.log("Pestaña "+current);
+              
+              
+
+            },
+            function(e){
+              console.error(e);
+            }
+        );
+    }
+
+  vm.searchGuides = function (){
+console.log(vm.currentTab);
+console.log(vm.textoBuscar);
+//recorrer cada guia y recorrer sus tags
+
+    }
+
   }
 })();
